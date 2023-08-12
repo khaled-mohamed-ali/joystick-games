924e9dfc73157e4a9ed8589661b103775a9948f1
khaled mohammed <dev.khaled.mohamed@gmail.com>
2023-08-12 11:18:45 +0300

    enhancement at recently added games

--- app/components/serverComponents/RecentlyAdded.jsx
+++ app/components/serverComponents/RecentlyAdded.jsx
@@ -15,6 +15,7 @@ const RecentlyAdded = async () => {
     const lastedGamesAdded = recentlyAddedItems.filter(ele => ele.release_date > specificDate[0]);
 
 
+
     return (
         <div>
             {lastedGamesAdded.map(({thumbnail, title, genre, short_description, id, platform},index) => {
@@ -23,7 +24,7 @@ const RecentlyAdded = async () => {
                         <div className='row   mainCard mx-0  rounded mb-4 justify-content-between '
                              style={{maxHeight: '115px'}}
                         >
-                            <div className='col-3' style={{maxWidth: '150px'}}>
+                            <div className='col-3 py-3 py-ms-0' style={{maxWidth: '150px'}}>
                                 <img className='align-self-center  productImg col-3' src={thumbnail} alt=""/>
                             </div>
                             <div className='col-6 align-self-center'>
@@ -31,7 +32,7 @@ const RecentlyAdded = async () => {
                                 <p className='text-truncate discrepColor mb-1'>{short_description}</p>
                                 <span className='fontSize4 genre badge-secondary text-dark mr-2 '>{genre}</span>
                             </div>
-                            <div className='col-3 d-flex justify-content-around  align-items-center pe-0'>
+                            <div className='col-3 d-flex justify-content-around  align-items-center  pe-ms-0 ps-ms-3 px-0'>
                                 <h5 className='discrepColor'>
                                     {platform == 'PC (Windows)' ? <BsWindows/> : <MdOutlineWebAsset className="fs-4"/>}
                                 </h5>
