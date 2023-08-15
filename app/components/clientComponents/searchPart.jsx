6c0267115b720cc4a9905506d3ba919df6fdf4de
khaled mohammed <dev.khaled.mohamed@gmail.com>
2023-08-15 10:38:02 +0300

    Search system improvement

--- app/components/clientComponents/searchPart.jsx
+++ app/components/clientComponents/searchPart.jsx
@@ -6,45 +6,47 @@ import useSWR from "swr";
 import GameCardByCategory from "@/app/components/serverComponents/gameCardByCategory";
 // import useSWR from 'swr'
 
-
+let timeout = null;
 const SearchPart = (props) => {
 
     const games = props.games;
-    const [searchInput, setSearchInput] = useState("");
     const [query, setQuery] = useState("");
+    const [filteredGames, setFilteredGames] = useState([]);
 
-    let filterdGames = games.filter(game => {
-        const gameTitle = game.title.toLowerCase();
-        return (
-            gameTitle.includes(searchInput)
-        )
-    })
+    const handleSearchInput = (event) => {
+        setQuery(event.target.value);
+    }
 
     useEffect(() => {
-        const timeOut = setTimeout(() => setSearchInput(query), 1000)
-        return () => clearTimeout((timeOut))
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            let gamesMatchesQuery = [];
+            if (query.trim().length) {
+                gamesMatchesQuery = games.filter(game => {
+                    const gameTitle = game.title.toLowerCase();
+                    return gameTitle.includes(query.toLowerCase());
+                });
+            }
+            setFilteredGames(gamesMatchesQuery);
+        }, 1000);
     }, [query])
-
-    if (query.length <= 0) {
-        filterdGames = [];
-    }
 
     return (
         <div className="row mb-5">
             <h2 className="mainColor"><BiSearchAlt2/> Find Games</h2>
             <hr className="discrepColor"/>
-            <div className="col-lg-5 px-2 px-lg-0">
+            <div className="col-lg-5 px-2">
                 <input type="text" id={style.searchInputStyle}
                        value={query}
-                       onChange={(e) => setQuery(e.target.value)}
+                       onChange={handleSearchInput}
                        className={`mt-2 mb-5 darkBgColor mainColor form-control`}
                        placeholder="Search for games"
-                       autocomplete="off"
+                       autoComplete="off"
                 />
             </div>
             <div className="col-12">
                 <div className="row">
-                    {filterdGames.map((game, index) => {
+                    {filteredGames.map((game, index) => {
                         return (
                             <div className="col-xl-3 col-md-4 col-12 mb-4" key={index}>
                                 <GameCardByCategory {...game}/>
